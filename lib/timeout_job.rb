a36346f795ec16a3d6298f872173f9e90adc46e9
ts-3156 <ts_3156@yahoo.co.jp>
2020-05-25 18:55:43 +0900

    Add TimeoutJob::Middleware

--- lib/timeout_job.rb
+++ lib/timeout_job.rb
@@ -1,6 +1,73 @@
+require 'timeout'
+require 'logger'
+
 require "timeout_job/version"
 
 module TimeoutJob
-  class Error < StandardError; end
-  # Your code goes here...
+  class Middleware
+    def call(worker, msg, queue, &block)
+      if worker.respond_to?(:timeout_in)
+        result = yield_with_timeout(worker.timeout_in, &block)
+
+        if timeout?
+          logger.info "job execution is timed out. timeout_in=#{worker.timeout_in} args=#{truncate(msg['args'].inspect)}"
+          perform_callback(worker, :after_timeout, args)
+          nil
+        else
+          result
+        end
+      else
+        yield
+      end
+    end
+
+    def yield_with_timeout(timeout_in, &block)
+      @timeout = false
+      ::Timeout.timeout(timeout_in) do
+        yield
+      end
+    rescue ::Timeout::Error => e
+      @timeout = true
+      nil
+    end
+
+    def timeout?
+      @timeout
+    end
+
+    def perform_callback(worker, callback_name, args)
+      if worker.respond_to?(callback_name)
+        parameters = worker.method(callback_name).parameters
+
+        begin
+          if parameters.empty?
+            worker.send(callback_name)
+          else
+            worker.send(callback_name, *args)
+          end
+        rescue ArgumentError => e
+          message = "The number of parameters of the callback method (#{parameters.size}) is not the same as the number of arguments (#{args.size})"
+          raise ArgumentError.new("#{self.class}:#{worker.class} #{message} callback_name=#{callback_name} args=#{args.inspect} parameters=#{parameters.inspect}")
+        end
+      end
+    end
+
+    def truncate(text, length: 100)
+      if text.length > length
+        text.slice(0, length)
+      else
+        text
+      end
+    end
+
+    def logger
+      if defined?(::Sidekiq)
+        ::Sidekiq.logger
+      elsif defined?(::Rails)
+        ::Rails.logger
+      else
+        ::Logger.new(STDOUT)
+      end
+    end
+  end
 end
--- test/timeout_job_test.rb
+++ test/timeout_job_test.rb
@@ -1,11 +1,54 @@
 require "test_helper"
 
 class TimeoutJobTest < Minitest::Test
-  def test_that_it_has_a_version_number
-    refute_nil ::TimeoutJob::VERSION
+
+  class Worker
   end
 
-  def test_it_does_something_useful
-    assert false
+  class TimeoutWorker
+    def timeout_in(*args)
+      10
+    end
+
+    def after_timeout(*args)
+      'callback'
+    end
+  end
+
+  def test_call
+    msg = {'args' => 'args'}
+
+    result = TimeoutJob::Middleware.new.call(Worker.new, msg, nil) { 'result' }
+    assert result == 'result'
+
+    result = TimeoutJob::Middleware.new.call(TimeoutWorker.new, msg, nil) { 'result' }
+    assert result == 'result'
+  end
+
+  def test_yield_with_timeout
+    timeout_in = TimeoutWorker.new.timeout_in
+    result = TimeoutJob::Middleware.new.yield_with_timeout(timeout_in) { 'result' }
+    assert result == 'result'
+  end
+
+  def test_perform_callback
+    args = 'args'
+    result = TimeoutJob::Middleware.new.perform_callback(Worker.new, :after_timeout, args)
+    assert result.nil?
+
+    args = 'args'
+    result = TimeoutJob::Middleware.new.perform_callback(TimeoutWorker.new, :after_timeout, args)
+    assert result == 'callback'
+  end
+
+  def test_truncate
+    result = TimeoutJob::Middleware.new.truncate('a' * 10)
+    assert result == 'a' * 10
+
+    result = TimeoutJob::Middleware.new.truncate('a' * 200)
+    assert result == 'a' * 100
+  end
+
+  def test_logger
   end
 end
